291e81dc138d5b1caac5af8d6177a99e61245de3
Tatyana Dovgan <tanya@iMac-Tatyana.local>
2016-08-29 17:29:39 +0300

    updated converting to float

--- ex1-sum1.py
+++ ex1-sum1.py
@@ -5,9 +5,8 @@ s = input()
 a = s.split()
 summa = 0
 try: 
-    for elem in a:
-        elem = float(elem)
+    for float(elem) for elem in a:
         summa += elem
 except ValueError:
-	print("This element: ", elem ," not number. All elements in array should be numbers")
+	print("This element: ", elem ," not number. All elements in array should be numbers. Only elements before this element are calculated in sum.")
 print('summa=',summa)
